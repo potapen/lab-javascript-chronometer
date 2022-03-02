307e93579722acc109f1273cc0eac470ec53a9c5
potapen <po@ta.pen>
2022-03-02 12:58:53 +0100

    almost done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,92 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
     // ... your code goes here
+    // console.log(callback)
+    if(callback === undefined){console.log('no callback defined')}
+    else{
+      this.intervalId = setInterval(callback, 1000)
+    }
   }
 
   getMinutes() {
     // ... your code goes here
+    //this.currentTime contains the numbers of seconds that has passed
+    // console.log('this.currentTime in s :', this.currentTime)
+    const numberMinute = Math.floor(this.currentTime/60)
+    // console.log('numberMinute: ',numberMinute)
+    return numberMinute
   }
 
   getSeconds() {
     // ... your code goes here
+    //this.currentTime contains the numbers of seconds that has passed
+    // console.log('this.currentTime in s :', this.currentTime)
+    const numberSecondsFromLastMinute = this.currentTime % 60
+    // console.log('numberSecondsFromLastMinute: ',numberSecondsFromLastMinute)
+    return numberSecondsFromLastMinute
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const outputString = value.toString().padStart(2, '0')
+    //console.log('padded outputString: ', outputString)
+    return outputString
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    //console.log('before reset :', this.currentTime)
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
+    const formattedTimeString = this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds())
+    return formattedTimeString
   }
 }
+
+
+/*
+💡 Hint 1: Keep in mind, if you pass a function declaration to the setInterval() method (by writing setInterval(function () {})),
+ the keyword this will not refer to the object chronometer, but to global scope. To enable referencing the chronometer by accessing this,
+  pass a function expression (a so-called arrow function) to the setInterval() method (by writing setInterval(() => {}) instead).
+*/
+
+
+/* TESTING
+//initiate time
+const myChrono = new Chronometer()
+const Date0 = Date.now()
+console.log('Date0 :', Date0)
+*/
+
+/* TESTING
+const dummyFunction = () => {console.log('hello')}
+myChrono.start(dummyFunction)
+myChrono.currentTime = 124
+console.log(myChrono.getMinutes())
+console.log(myChrono.getSeconds())
+console.log('myChrono.computeTwoDigitNumber(1) :', myChrono.computeTwoDigitNumber(1))
+console.log('myChrono.computeTwoDigitNumber(11) :', myChrono.computeTwoDigitNumber(11))
+myChrono.reset()
+myChrono.currentTime = 201
+console.log('myChrono.currentTime: ', myChrono.currentTime)
+console.log('myChrono.split(): ', myChrono.split())
+*/
+
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+
+const Date0 = Date.now()
 const chronometer = new Chronometer();
 
 // get the buttons:
@@ -54,12 +56,64 @@ function setResetBtn() {
   // ... your code goes here
 }
 
+function updateTime() {
+  const elapsedTime = Date.now() - Date0
+  console.log('elapsedTime: ', elapsedTime)
+  chronometer.currentTime = Math.floor(elapsedTime/1000)
+  console.log('chronometer.currentTime:', chronometer.currentTime)
+  const seconds = chronometer.getSeconds()
+  const minutes = chronometer.getMinutes()
+  //console.log(`seconds: ${seconds}, minutes: ${minutes}`)
+  const secondsDigit0 = chronometer.computeTwoDigitNumber(seconds)[0]
+  const secondsDigit1 = chronometer.computeTwoDigitNumber(seconds)[1]
+  const minutesDigit0 = chronometer.computeTwoDigitNumber(minutes)[0]
+  const minutesDigit1 = chronometer.computeTwoDigitNumber(minutes)[1]
+  console.log(minutesDigit0, minutesDigit1, secondsDigit0,secondsDigit1)
+  minDecElement.innerText = minutesDigit0
+  minUniElement.innerText = minutesDigit1
+  secDecElement.innerText = secondsDigit0
+  secUniElement.innerText = secondsDigit1
+}
+
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
   // ... your code goes here
+  //you can't directly modify btnLeftElement.classList by assigning another array
+  if (btnLeftElement.classList.contains('start')){
+    btnLeftElement.classList.toggle("start")
+    btnLeftElement.classList.toggle("stop")
+    btnLeftElement.innerText = "STOP"
+    btnRightElement.classList.toggle("reset")
+    btnRightElement.classList.toggle("split")
+    btnRightElement.innerText = "SPLIT"
+    chronometer.start(updateTime,1000)
+  } else if (btnLeftElement.classList.contains('stop')){
+    btnLeftElement.classList.toggle("stop")
+    btnLeftElement.classList.toggle("start")
+    btnLeftElement.innerText = "START"
+    btnRightElement.classList.toggle("split")
+    btnRightElement.classList.toggle("reset")
+    btnRightElement.innerText = "RESET"
+    chronometer.stop()
+  }
+
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
   // ... your code goes here
+  if (btnRightElement.classList.contains('split')){
+    console.log('appending li')
+    const newLi = document.createElement("li")
+    const textLi = chronometer.computeTwoDigitNumber(chronometer.getMinutes()) + ':' + chronometer.computeTwoDigitNumber(chronometer.getSeconds())
+    newLi.innerText = textLi
+    splitsElement.appendChild(newLi)
+  }
+  if (btnRightElement.classList.contains('reset')){
+    minDecElement.innerText = 0
+    minUniElement.innerText = 0
+    secDecElement.innerText = 0
+    secUniElement.innerText = 0
+    chronometer.currentTime = 0
+  }
 });
